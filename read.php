df966ce7468413cb10487087f94e323788637467
ejiexe <116974415+ejiexe@users.noreply.github.com>
2022-10-30 11:12:06 +0100

    Testing

--- read.php
+++ read.php
@@ -11,7 +11,7 @@
     $conn = mysqli_init();
     mysqli_ssl_set($conn,NULL,NULL,$sslcert,NULL,NULL);
     if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL)){
-        die('Failed to connect to MySQL: '.mysqli_connect_error());
+        die('Failed  xxxxxxxxxxxxxx to connect to MySQL: '.mysqli_connect_error());
     }
 
     //Test if table exists
