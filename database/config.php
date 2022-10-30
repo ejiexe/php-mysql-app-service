8916438f7a4d4b1b1a36ed65222d04cae174bde7
ejiexe <116974415+ejiexe@users.noreply.github.com>
2022-10-30 17:22:22 +0100

    new

--- database/config.php
+++ database/config.php
@@ -2,8 +2,8 @@
 
 // Configuration for database connection
 
-$host       = getenv('britishnigerianacademy.mysql.database.azure.com');
-$username   = getenv('bnaictmanager');
-$password   = getenv('Bw203app$');
-$db_name     = getenv('sampledb');
+$host       = getenv('DB_HOST');
+$username   = getenv('DB_USERNAME');
+$password   = getenv('DB_PASSWORD');
+$db_name     = getenv('DB_DATABASE');
 $sslcert    = "ssl/DigiCertGlobalRootCA.crt.pem";
